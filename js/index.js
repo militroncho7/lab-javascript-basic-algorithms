828d5490a5d35e401547afad2cfd3dac32cb2533
militroncho7 <victor@MacBook-Air-de-Victor.local>
2019-11-10 19:06:38 +0100

    Iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
+//Initial Test
+
+console.log("I'm ready!");
+
+console.log("================================================");
+
 // Iteration 1: Names and Input
 
+let hacker1 = "Víctor";
+
+console.log("The driver's name is", hacker1);
+
+let hacker2 = "Google";
+
+console.log("The navigator's name is", hacker2);
+
+console.log("================================================");
 
 // Iteration 2: Conditionals
 
